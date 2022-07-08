f72977aebb2ba84148891058f0113e521747e4bc
Jake Miller <jakemiller.bc@gmail.com>
2022-07-08 11:30:02 -0500

    commit

---
+++ Week 1/First JSX/first-jsx/src/components/Hello.js
@@ -0,0 +1,17 @@
+
+const Hello = () => {
+    return(
+        <div className="App">
+            <h1>Hello Dojo!</h1>
+            <h4>Things I need to do</h4>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Hello;
