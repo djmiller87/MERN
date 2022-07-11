2b2a383c7eb074aa2621ebb2a423465863e97e40
djmiller87 <jakemiller.bc@gmail.com>
2022-07-10 22:38:58 -0500

    commit

---
+++ Week 1/prop-it-up/src/components/PersonCard.js
@@ -0,0 +1,13 @@
+
+const PersonCard = (props) => {
+    const { firstName, lastName, age, hairColor } = props;
+    return (
+        <>
+            <h1>{lastName}, {firstName}</h1>
+            <p>Age: {age}</p>
+            <p>Hair Color: {hairColor}</p>
+        </>
+    )
+}
+
+export default PersonCard;
